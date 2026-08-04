8d2a278b0c645d58da2a806f3196feb0f35b12a8
Mr.Pilu <89413412+MrPilu@users.noreply.github.com>
2026-08-04 16:47:28 +0100

    Delete array.py

--- array.py
+++
@@ -1,9 +0,0 @@
-# Create an array (list) with assigned values
-numbers = [10, 20, 30, 40, 50]
-
-# Display the array
-print("Array:", numbers)
-
-# Display individual elements
-print("First element:", numbers[0])
-print("Last element:", numbers[4])
