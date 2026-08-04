184da56261ea982224ed4917a3e3a9046e2c406d
Mr.Pilu <89413412+MrPilu@users.noreply.github.com>
2026-08-04 15:13:15 +0100

    Create print.py

---
+++ print.py
@@ -0,0 +1,9 @@
+# Create an array (list) with assigned values
+numbers = [10, 20, 30, 40, 50]
+
+# Display the array
+print("Array:", numbers)
+
+# Display individual elements
+print("First element:", numbers[0])
+print("Last element:", numbers[4])
